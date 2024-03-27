a0ca78f74bb489b03c4dfc8bed390cc4c4753609
unknown <gourav052003@gmail.com>
2024-03-27 11:36:34 +0530

    Pearson test  added

--- Hypothesis tests/Parameteric-Tests/PearsonR-test.py
+++ Hypothesis tests/Parameteric-Tests/PearsonR-test.py
@@ -1,3 +1,22 @@
+
+
+import pandas as pd
+import math
+import warnings
+warnings.filterwarnings('ignore')
+
+CARDIO_BASE_df = pd.read_csv('Data\\cardio_base.csv',delimiter=',')
+print(CARDIO_BASE_df.head())
+
+CARDIO_BASE_df["age"] = CARDIO_BASE_df["age"].apply(lambda x:math.floor(x/365))
+print(CARDIO_BASE_df.head())
+
+import seaborn as sns
+
+# checking for normally distributed data beacuse parametric test give accurate results with normally distributes data 
+# although parameteric test can also be done for non - normal distributed data
+sns.distplot(CARDIO_BASE_df["weight"])
+
 from scipy.stats import pearsonr
 
 # alternative{‘two-sided’, ‘greater’, ‘less’}, optional
